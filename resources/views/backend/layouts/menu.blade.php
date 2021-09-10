312d5422a620aa233c6e4125fc0308888db4a882
thanhle <thanhle09t2bkdn@gmail.com>
2021-09-10 16:41:57 +0700

    menu

--- resources/views/backend/layouts/menu.blade.php
+++ resources/views/backend/layouts/menu.blade.php
@@ -58,32 +58,3 @@
     </ul>
 </li>
 
-<li class="nav-item has-treeview">
-    <a href="#" class="nav-link">
-        <i class="nav-icon far fa-address-card"></i>
-        <p>
-            Memberships
-            <i class="fas fa-angle-right right"></i>
-        </p>
-    </a>
-    <ul class="nav nav-treeview">
-        <li class="nav-item">
-            <a href="" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Genres</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Organizations</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Memberships</p>
-            </a>
-        </li>
-    </ul>
-</li>
