fe29092c8769561e1d8c5c7404c4c8ee2683b63c
thanhle <thanhle09t2bkdn@gmail.com>
2021-09-14 17:50:47 +0700

    search

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,9 +8,36 @@ use App\Models\User;
 
 class UserRepository extends BaseRepository
 {
+    /**
+     * Attribute searchable
+     *
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'id' => ['column' => 'users.id', 'operator' => '='],
+        'name' => ['column' => 'users.name', 'operator' => 'like'],
+        'email' => ['column' => 'users.email', 'operator' => 'like'],
+        'role' => ['column' => 'users.role', 'operator' => '='],
+    ];
+
+
     public function __construct(User $model)
     {
         $this->model = $model;
+    }
+
+    /**
+     * searchFromParams
+     *
+     * @param $request
+     *
+     * @return mixed
+     */
+    public function searchFromRequest($request)
+    {
+        return $this->search($request->only(array_keys($this->fieldSearchable)))
+            ->orderBy('created_at', 'desc')
+            ->paginate();
     }
 
 }
